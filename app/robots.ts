8a12f0e314d9a677ab9a826fe339366f8dbbd1e1
Anurag Mishra <Anurag@Anurag-Mishra.altruistindia.com>
2026-07-06 18:30:41 +0530

    Technical SEO implementation

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://tiger365online.com/sitemap.xml',
+  };
+}
